ca4ab73417035d1be15bbfd80e5f0a1e5151f838
Joe O <j0liverko@gmail.com>
2019-10-18 11:59:56 -0500

    Mvp

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 export default function Header() {
   return (
     <header className="ui centered">
       <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <Link to='/WelcomePage'>
+        Home
+      </Link>
+      <Link to='/CharacterList'>
+        Characters!
+      </Link>
     </header>
   );
 }
